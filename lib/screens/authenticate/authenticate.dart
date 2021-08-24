2341bb752abc8a148bfecd45b923bb71b0ce2390
Eng Mouaz M. Al-Shahmeh <86870601+Eng-Mouaz-M-AlShahmeh@users.noreply.github.com>
2021-08-24 19:05:05 +0300

    Update authenticate.dart

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,5 @@
+// Eng Mouaz M. Al-Shahmeh
+ 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/screens/authenticate/register.dart';
 // import 'package:flutter_application_2/screens/authenticate/register.dart';
